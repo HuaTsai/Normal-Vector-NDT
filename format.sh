9fdf8fb49d49d7353a44e6d3c3dccec330a26a52
HuaTsai <a0910666734@gmail.com>
2022-04-07 23:24:37 +0800

    style: new line in end of file

--- format.sh
+++ format.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 find . -regex '\.\/src\/.*\.\(h\|hpp\|cc\|cpp\)' -exec clang-format -style=file -i {} \;
+
+files=`find . -regex '\.\/src\/.*\.\(h\|hpp\|cc\|cpp\)'`
+for file in $files
+do
+    if [ -n "$(tail -c 1 "$file")" ]
+    then
+        echo "" >> $file
+    fi
+done
